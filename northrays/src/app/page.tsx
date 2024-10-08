53ac79c7987294b770d17868074535a7cc0e22db
areeba75 <areebaars1@gmail.com>
2024-10-08 06:13:48 -0700

    added

--- northrays/src/app/page.tsx
+++ northrays/src/app/page.tsx
@@ -1,14 +1,49 @@
-import Image from "next/image";
-import aboutStyle from "./page.module.scss";
+import React from "react";
+import "./page.scss"; // Import your Sass file
 
-export default function Home() {
+const ProductCard = () => {
   return (
-    <div className={aboutStyle.page}>
+    <section>
+      
+      <Product
+        image="https://via.placeholder.com/150"
+        title="Product Title"
+        description=" This is a great product that will improve your life in two amazing
+        ways."
+        price={29.99}
+      /> 
+       <Product
+      image="https://via.placeholder.com/150"
+      title="This is Title"
+      description=" This is a  product that will improve your life in two amazing
+      ways."
+      price={1111.99}
+    />
+    </section>
+  );
+};
 
-      <p className={aboutStyle.textgreen}>
-        h world
-      </p>
-     
+export default ProductCard;
+
+interface prdprops {
+  image: string;
+  title: string;
+  description: string;
+  price: number;
+}
+const Product = ({ image, title, description, price }: prdprops) => {
+  return (
+    <div className="product-card">
+      <img
+        src={image}// Replace with your image URL
+      />
+      <div className="product-card__content">
+        <h2 className="product-card__title">{title}</h2>
+        <p className="product-card__description">
+       {description}
+        </p>
+        <span className="product-card__price">${price}</span>
+      </div>
     </div>
   );
-}
+};
